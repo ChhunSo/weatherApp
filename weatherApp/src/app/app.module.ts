77b9ab8ae7994674ad4ce0ca5673bdcd85496573
ChhunSo <Chhun@gohero.me>
2017-08-23 11:56:49 -0700

    create weather-item component and weather-list component + add components to declarations in app module"

--- weatherApp/src/app/app.module.ts
+++ weatherApp/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { WeatherListComponent } from '../dev/weather/weather-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherListComponent
   ],
   imports: [
     BrowserModule,
---
+++ weatherApp/src/dev/weather/weather-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: 'weather-item' //Standard is to prefix selectors with 'my' to not interfere with packages,
+	templateUrl:'./weather-item.html',
+	styleUrls: ['./weather-item.css']
+})
+
+export class WeatherItemComponent {
+
+}
